f867e129243d4110b53e48aede9b3195ba564fb3
kent1 <genetquentin@gmail.com>
2024-10-29 22:31:11 +0100

    🔧 Refactor ResetPassword feature; replace component, enhance form validation, and improve error handling in password reset process

---
+++ vinylkeeper_front/src/pages/ResetPassword/ResetPassword.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import styles from "../../styles/pages/Landpage.module.scss";
+import useDetectMobile from "../../hooks/useDetectMobile";
+
+import Footer from "@components/Footer/Footer";
+import { Typography } from "@mui/material";
+import videoBackground from "../../assets/landpageBackground.mp4";
+import ResetPasswordForm from "@components/ResetPassword/ResetPasswordForm";
+
+export default function ResetPassword() {
+  const { isMobile } = useDetectMobile();
+
+  const [openTermsModal, setOpenTermsModal] = useState<boolean>(false);
+
+  return (
+    <>
+      <div className={styles.globalContainer}>
+        {isMobile ? (
+          <div className={styles.imgMobileBackgroundVinylKeeper}></div>
+        ) : (
+          <video
+            autoPlay
+            muted
+            loop
+            className={styles.videoBackgroundVinylKeeper}
+          >
+            <source src={videoBackground} type="video/mp4" />
+          </video>
+        )}
+        <Typography variant="h1" className={styles.titleNormal}>
+          Vinyl Keeper
+        </Typography>
+        <div className={styles.textContainer}>
+          <p className={styles.textTypo}>
+            Ready to reset your password&nbsp;?
+            <br /> Let’s get started just below&nbsp;!
+          </p>
+
+          <div className={styles.actionContainer}>
+            <ResetPasswordForm />
+          </div>
+        </div>
+        <Footer
+          setOpenTermsModal={setOpenTermsModal}
+          openTermsModal={openTermsModal}
+        />
+      </div>
+    </>
+  );
+}
