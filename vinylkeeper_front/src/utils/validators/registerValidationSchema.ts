c5073a344235032770c7808203efcab05228b3a4
kent1 <genetquentin@gmail.com>
2024-10-31 21:39:19 +0100

    ⚡️ Refactoring code

---
+++ vinylkeeper_front/src/utils/validators/registerValidationSchema.ts
@@ -0,0 +1,39 @@
+import * as yup from "yup";
+import {
+  emailValidator,
+  passwordAtLeast4,
+  passwordWithLetter,
+  passwordWithNumber,
+} from "@utils/Regex";
+
+export const registerValidationSchema = yup.object({
+  username: yup
+    .string()
+    .min(3, "Username must contain at least 3 characters")
+    .matches(/^[a-z0-9]+$/, "Must be lowercase")
+    .required("Enter your username"),
+  email: yup
+    .string()
+    .test("email", "Email not valid", (value) =>
+      value ? emailValidator.test(value) : false
+    )
+    .min(3, "Email must contain at least 4 characters.")
+    .required("You must enter your Email."),
+  isAcceptedTerms: yup
+    .boolean()
+    .required("You must accept terms to use Vinyl Keeper"),
+  password: yup
+    .string()
+    .required("Enter your password.")
+    .matches(passwordWithLetter, "Your password must contain a letter")
+    .matches(passwordWithNumber, "Your password must contain a number")
+    .matches(
+      passwordAtLeast4,
+      "Your password must contain at least 4 characters"
+    ),
+  passwordBis: yup
+    .string()
+    .required("Confirm your password.")
+    .oneOf([yup.ref("password"), ""], "It must be the same as the password"),
+  timezone: yup.string().required(),
+});
