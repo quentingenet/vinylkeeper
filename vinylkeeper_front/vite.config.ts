4500ac5f6414f846cd8afeb6e6aa9107ad140fc7
kent1 <genetquentin@gmail.com>
2024-10-31 18:19:44 +0100

    ⬆️ Upgrading front depedencies

--- vinylkeeper_front/vite.config.ts
+++ vinylkeeper_front/vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: '',
+        api: "modern",
+        additionalData: "",
       },
     },
   },
   resolve: {
     alias: {
-      '@pages': '/src/pages',
-      '@components': '/src/components',
-      '@styles': '/src/styles',
-      '@models': '/src/models',
-      '@services': '/src/services',
-      '@utils': '/src/utils',
-      '@routing': '/src/routing',
-      '@contexts': '/src/contexts',
-      '@hooks': '/src/hooks',
-      '@themes': '/src/styles/themes',
-    }
-  }
+      "@pages": "/src/pages",
+      "@components": "/src/components",
+      "@styles": "/src/styles",
+      "@models": "/src/models",
+      "@services": "/src/services",
+      "@utils": "/src/utils",
+      "@routing": "/src/routing",
+      "@contexts": "/src/contexts",
+      "@hooks": "/src/hooks",
+      "@themes": "/src/styles/themes",
+    },
+  },
 });
