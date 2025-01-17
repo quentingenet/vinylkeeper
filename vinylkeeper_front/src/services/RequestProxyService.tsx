867469ffddc37029c3fccc4c8d0a75c8b3867f82
kent1 <genetquentin@gmail.com>
2025-01-17 19:06:52 +0100

    🔧 Fix import path for IRequestToSend in RequestProxyService.tsx

--- vinylkeeper_front/src/services/RequestProxyService.tsx
+++ vinylkeeper_front/src/services/RequestProxyService.tsx
@@ -1,6 +1,6 @@
 import requestService from "@utils/RequestService";
 import { API_VK_URL } from "@utils/GlobalUtils";
-import { IRequestToSend } from "@models/IRequestPRoxy";
+import { IRequestToSend } from "@models/IRequestProxy";
 
 export const searchProxy = (requestToSend: IRequestToSend) => {
   return requestService({
