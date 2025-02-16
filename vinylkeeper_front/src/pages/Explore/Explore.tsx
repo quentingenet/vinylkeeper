30e43abb6deeb60457cac3383a9f6bf943218694
kent1 <genetquentin@gmail.com>
2025-02-16 17:10:37 +0100

    📝 Add Explore Page Placeholder with Feature Roadmap
    - Created initial Explore page component
    - Added placeholder list of planned features for personalized music exploration
    - Outlined future implementation of scrollable collection and recommendation banners

--- vinylkeeper_front/src/pages/Explore/Explore.tsx
+++ vinylkeeper_front/src/pages/Explore/Explore.tsx
@@ -1,3 +1,20 @@
 export default function Explore() {
-  return <div>Explore</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          Un bandeau avec scrollX pour explorer les collections PUBLIC partagée
+          par les users
+        </li>
+        <li>
+          Un bandeau avec scrollX pour avoir des suggestions albums personnalisé
+          via scikitLearn
+        </li>
+        <li>
+          Un bandeau avec scrollX pour avoir des suggestions artistes
+          personnalisé via scikitLearn
+        </li>
+      </ul>
+    </div>
+  );
 }
