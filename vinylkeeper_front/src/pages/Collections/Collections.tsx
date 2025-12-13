812d2bf3c5c6dbd7b4a8922571c074c03e7fd6cf
kent1 <genetquentin@gmail.com>
2025-12-13 14:34:44 +0100

    🐛Fix build error

--- vinylkeeper_front/src/pages/Collections/Collections.tsx
+++ vinylkeeper_front/src/pages/Collections/Collections.tsx
@@ -6,7 +6,10 @@ import PaginationWithEllipsis from "@components/UI/PaginationWithEllipsis";
 import VinylSpinner from "@components/UI/VinylSpinner";
 import { useState, useCallback, useMemo } from "react";
 import useDetectMobile from "@hooks/useDetectMobile";
-import { CollectionResponse } from "@services/CollectionApiService";
+import {
+  CollectionResponse,
+  CollectionListItemResponse,
+} from "@services/CollectionApiService";
 import ModalCollection from "@components/Collections/ModalCollection";
 import { ITEMS_PER_PAGE } from "@utils/GlobalUtils";
 import { useCollections } from "@hooks/useCollections";
@@ -57,7 +60,7 @@ export default function Collections() {
     setModalState({
       isOpen: true,
       isUpdating,
-      collection,
+      collection: collection as CollectionResponse | undefined,
       isPublic: false,
     });
   };
@@ -66,7 +69,9 @@ export default function Collections() {
     setModalState((prev) => ({ ...prev, isOpen: false, isUpdating: false }));
   };
 
-  const handleCollectionClick = (collection: CollectionResponse) => {
+  const handleCollectionClick = (
+    collection: CollectionResponse | CollectionListItemResponse
+  ) => {
     navigate(
       EGlobalUrls.COLLECTION_DETAILS.replace(":id", collection.id.toString())
     );
@@ -170,7 +175,12 @@ export default function Collections() {
                 handleSwitchVisibility(collection.id, newIsPublic);
               }}
               handleOpenModalCollection={(collection) => {
-                handleOpenModalCollection(true, collection);
+                if ("albums" in collection && "artists" in collection) {
+                  handleOpenModalCollection(
+                    true,
+                    collection as CollectionResponse
+                  );
+                }
               }}
               onCollectionClick={handleCollectionClick}
               isOwner={true}
